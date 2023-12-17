b044abd8d2720586a69ae2b543792a40b6a2d97e
Victor Cano Gil <vcanogil@gmail.com>
2023-12-17 14:38:21 -0500

    change test size

--- models/graph/helpers.py
+++ models/graph/helpers.py
@@ -75,7 +75,7 @@ def make_train_val_data(csv_file_cov,
         df = df.drop_duplicates(subset=["SMILES"])
 
         df_train, df_val = train_test_split(df,
-                                            test_size=0.1,
+                                            test_size=0.05,
                                             shuffle=True,
                                             stratify=df.covalent.values,
                                             random_state=RANDOM_STATE)
