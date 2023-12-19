90694c3c43314d5c67f18584719613de5633976b
Victor Cano Gil <vcanogil@gmail.com>
2023-12-18 22:25:54 -0500

    update models

--- models/graph/helpers.py
+++ models/graph/helpers.py
@@ -76,7 +76,7 @@ def make_train_val_data(csv_file_cov,
         df = df.drop_duplicates(subset=["SMILES"])
 
         df_train, df_val = train_test_split(df,
-                                            test_size=0.05,
+                                            test_size=0.1,
                                             shuffle=True,
                                             stratify=df.covalent.values,
                                             random_state=RANDOM_STATE)
