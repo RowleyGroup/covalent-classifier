e699e75f0d902da8a79f7384b1a86f72953604f8
Victor Cano Gil <vcanogil@gmail.com>
2024-01-11 17:03:20 -0500

    update pred function

--- models/graph/make_prediction.py
+++ models/graph/make_prediction.py
@@ -1,9 +1,13 @@
 import argparse
 import tensorflow as tf
 from helpers import encode
+from rdkit import Chem
 
 
 def make_prediction(input_structure, model="./saved_models/GCNII"):
+    mol = Chem.MolFromSmiles(input_structure)
+    if not mol:
+        raise ValueError("Failed to generate mol from smiles")
     model = tf.keras.models.load_model(model)
     graph = encode(input_structure)
     return model.predict(graph)
@@ -11,7 +15,7 @@ def make_prediction(input_structure, model="./saved_models/GCNII"):
 
 def main():
     argParser = argparse.ArgumentParser()
-    argParser.add_argument("inchi_string", help="InChI or SMILES string")
+    argParser.add_argument("inchi_string", help="SMILES string")
     args = argParser.parse_args()
     print(make_prediction(args.inchi_string), flush=True)
 
